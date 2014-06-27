83b2206aefd960ca9fcc2c2f69439f14320aa13f
Caridy Patino <caridy@gmail.com>
2014-06-27 14:51:56 -0400

    getting server and client implementation of the loader ready

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-if (typeof Promise === 'undefined') {
-    // patching the runtime if `Promise` is not present
-    global.Promise = require('ypromise');
-}
-if (typeof System === 'undefined') {
-    // patching the runtime if `System` is not present.
-    require('./micro-loader.js');
+/* jslint node:true */
+
+if (global.System) {
+  throw new Error("Conflicts with the global `System` definition, use `es6-micro-loader/lib/system` instead.");
 }
 
-module.exports = global.System;
+module.exports = global.System = require("./lib/system");
