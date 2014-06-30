280b3c605ffafab390530d0ef2249a2ee93785bd
Caridy Patino <caridy@gmail.com>
2014-06-30 17:43:36 -0400

    adding second round of benchmarks

--- tests/benchmark/src/main.js
+++ tests/benchmark/src/main.js
@@ -5,3 +5,8 @@ import isOdd from "./odds";
 export function checkForOdd(n) {
     return isOdd(n);
 }
+
+// this is needed for `bundle` output
+if (typeof global !== 'undefined') {
+  global.checkForOdd = checkForOdd;
+}
